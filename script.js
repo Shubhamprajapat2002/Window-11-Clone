b7ac049e7c909c7b695d320fe0c5711253907e16
shubhamprajapat2002 <shubhamprajapat0903@gmail.com>
2022-12-18 16:15:53 +0530

    window 11 clone

---
+++ script.js
@@ -0,0 +1,144 @@
+let ABC = document.getElementsByClassName("ABC")[0]
+let startmenu = document.getElementsByClassName("startmenu")[0]
+
+ABC.addEventListener("click", ()=>{
+    console.log("clicked");
+    if(startmenu.style.bottom == "40px"){
+        startmenu.style.bottom = "-655px"
+    }
+    else{
+        startmenu.style.bottom = "40px"
+    }
+});
+//searchbar
+let BBC = document.getElementsByClassName("BBC")[0]
+let searchbar = document.getElementsByClassName("searchbar")[0]
+
+BBC.addEventListener("click", ()=>{
+    console.log("clicked");
+    if(searchbar.style.bottom == "40px"){
+        searchbar.style.bottom = "-655px"
+    }
+    else{
+        searchbar.style.bottom = "40px"
+    }
+});
+//edgebar
+let CBC = document.getElementsByClassName("CBC")[0]
+let edgebar = document.getElementsByClassName("edgebar")[0]
+
+CBC.addEventListener("click", ()=>{
+    console.log("clicked");
+    if(edgebar.style.bottom == "20px"){
+        edgebar.style.bottom = "-865px"
+    }
+    else{
+        edgebar.style.bottom = "20px"
+    }
+});
+//store
+let DBC = document.getElementsByClassName("DBC")[0]
+let store = document.getElementsByClassName("store")[0]
+
+DBC.addEventListener("click", ()=>{
+    console.log("clicked");
+    if(store.style.bottom == "20px"){
+        store.style.bottom = "-865px"
+    }
+    else{
+        store.style.bottom = "20px"
+    }
+});
+//arrow
+let EBC = document.getElementsByClassName("EBC")[0]
+let arrow = document.getElementsByClassName("arrow")[0]
+
+EBC.addEventListener("click", ()=>{
+    console.log("clicked");
+    if(arrow.style.bottom == "35px"){
+        arrow.style.bottom = "-865px"
+    }
+    else{
+        arrow.style.bottom = "35px"
+    }
+});
+//battery
+let FBC = document.getElementsByClassName("FBC")[0]
+let battery = document.getElementsByClassName("battery")[0]
+
+FBC.addEventListener("click", ()=>{
+    console.log("clicked");
+    if(battery.style.bottom == "30px"){
+        battery.style.bottom = "-865px"
+    }
+    else{
+        battery.style.bottom = "30px"
+    }
+});
+//wifi
+let GBC = document.getElementsByClassName("GBC")[0]
+let wifi = document.getElementsByClassName("wifi")[0]
+
+GBC.addEventListener("click", ()=>{
+    console.log("clicked");
+    if(wifi.style.bottom == "30px"){
+        wifi.style.bottom = "-865px"
+    }
+    else{
+        wifi.style.bottom = "30px"
+    }
+
+
+});
+//sound
+let HBC = document.getElementsByClassName("HBC")[0]
+let sound = document.getElementsByClassName("sound")[0]
+
+HBC.addEventListener("click", ()=>{
+    console.log("clicked");
+    if(sound.style.bottom == "30px"){
+        sound.style.bottom = "-865px"
+    }
+    else{
+        sound.style.bottom = "30px"
+    }
+});
+//bluetooth
+let IBC = document.getElementsByClassName("IBC")[0]
+let bluetooth = document.getElementsByClassName("bluetooth")[0]
+
+IBC.addEventListener("click", ()=>{
+    console.log("clicked");
+    if(bluetooth.style.bottom == "34px"){
+        bluetooth.style.bottom = "-865px"
+    }
+    else{
+        bluetooth.style.bottom = "34px"
+    }
+});
+//eng
+let JBC = document.getElementsByClassName("JBC")[0]
+let eng = document.getElementsByClassName("eng")[0]
+
+JBC.addEventListener("click", ()=>{
+    console.log("clicked");
+    if(eng.style.bottom == "30px"){
+        eng.style.bottom = "-865px"
+    }
+    else{
+        eng.style.bottom = "30px"
+    }
+});
+//time
+let KBC = document.getElementsByClassName("KBC")[0]
+let time = document.getElementsByClassName("time")[0]
+
+KBC.addEventListener("click", ()=>{
+    console.log("clicked");
+    if(time.style.bottom == "30px"){
+        time.style.bottom = "-865px"
+    }
+    else{
+        time.style.bottom = "30px"
+    }
+});
